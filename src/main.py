8c973a67abccec45c31e210bcfcbdf642403fb1b
Shourjjo_Majumder <shourjjomajum@outlook.com>
2022-07-26 15:02:03 +0530

    Workflows and src

---
+++ src/main.py
@@ -0,0 +1,27 @@
+
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+  
+  
+# Replace this with your 
+# current version
+kivy.require('2.1.0')  
+  
+# Defining a class
+class MyFirstKivyApp(App):
+      
+    # Function that returns 
+    # the root widget
+    def build(self):
+          
+        # Label with text Hello World is 
+        # returned as root widget
+        return Label(text ="Hello World !")          
+  
+  
+# Here our class is initialized
+# and its run() method is called. 
+# This initializes and starts 
+# our Kivy application.
+MyFirstKivyApp().run()         
